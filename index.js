321101e1051c967b588e8e68be6e44f5f10df221
Wendy Wigg <wiggwendy@gmail.com>
2019-06-15 19:34:29 -0700

    able to add new tasks

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render('index');
+// the task array with initial placeholders for added task
+var task = ["buy socks", "practice with node.js"];
+
+// adding new task
+app.post('/addtask', function(req, res) {
+    var newTask = req.body.newtask;
+    // add the new task from the post route into the array
+    task.push(newTask);
+    // after adding to the array go back to the root route
+    res.redirect("/");
 });
+
+// render the ejs and display added task, task(index.ejs) = task(array)
+app.get("/", function (req, res) {
+    res.render("index", { task: task});
+});
+
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
